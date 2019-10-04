003555e976fdabbfa67d17907de827802f4e48b2
Rupsa Saha <rupsajina@gmail.com>
2019-10-04 18:01:45 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ function get_reflection(cox,coy,angle){
 /* Mouse Capturing Work */
 canvas.addEventListener('mousemove', function(e) {
   mouse.x = e.pageX - this.offsetLeft-w/2;
-  mouse.y = e.pageY - this.offsetTop+h/4;//-h/2;
+  mouse.y = e.pageY - this.offsetTop-h/4;//-h/2;
 }, false);
 
 ctx.strokeStyle = "red";
